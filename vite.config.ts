3116f1ad356f6a7d5a7aa144627126b7266a46b7
Vojta Franek <vojtech.franek@livesystems.cz>
2025-06-08 22:22:20 +0200

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    base: process.env.VITE_BASE_URL || '/',
+    base: '/kniho-hlod-frontend/',
     build:{
       outDir:'dist'
     }
