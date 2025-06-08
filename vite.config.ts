d23765f419faefb96775c2f19b560793f9502c50
Vojta Franek <vojtech.franek@livesystems.cz>
2025-06-08 16:52:47 +0200

    github pages init

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'kniho-hlod-frontend',
     plugins: [
         vue(),
         Components({
