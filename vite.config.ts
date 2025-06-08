4963a6837f6119a372355989cab9cf5baf7b80ef
Vojta Franek <vojtech.franek@livesystems.cz>
2025-06-08 23:10:29 +0200

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    base: '/kniho-hlod-frontend/login',
+    base: '/kniho-hlod-frontend/',
     build:{
       outDir:'dist'
     }
